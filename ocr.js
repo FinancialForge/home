d2f82a96f49c42a089435f02b781054ea3fc5f45
LeonardLamparter <88286394+Code-With-Marley@users.noreply.github.com>
2024-10-28 10:45:13 -0400

    Update ocr.js without apikey
    Add the OCR code without the apikey

--- ocr.js
+++ ocr.js
@@ -1,1 +1,58 @@
-// OCR FILE
+function toBase64(file) {
+
+ return new Promise((resolve, reject) => {
+
+   const reader = new FileReader();
+
+   reader.readAsDataURL(file);
+
+   reader.onload = () => resolve(reader.result);
+
+   reader.onerror = error => reject(error);
+
+ });
+
+}
+
+function OCR(image) {
+
+// Define the URL and data
+
+const url = 'https://api.ocr.space/parse/image’;
+
+const data = {
+
+  apikey: ‘’
+
+  base64Image: toBase64(image)
+
+};
+
+// Define the options
+
+const options = {
+
+  method: 'POST',
+
+  headers: {
+
+    'Content-Type': 'application/json'
+
+  },
+
+  body: JSON.stringify(data)
+
+};
+
+
+// Send the request
+
+fetch(url, options)
+
+  .then(response => response.json())
+
+  .then(data => return data)
+
+  .catch(error => return {error:true});
+
+}
