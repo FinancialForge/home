4dfbdbfc39c425e685919141af052d57291f4d43
LeonardLamparter <88286394+Code-With-Marley@users.noreply.github.com>
2024-11-06 09:32:26 -0400

    Rewrote ocr.js

--- ocr.js
+++ ocr.js
@@ -1,58 +1,39 @@
-function toBase64(file) {
+async function OCR(imageInput) {
+  function toBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e) => resolve(e.target.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  }
+  
+  let b64 = '';  
+  if (imageInput.files.length > 0) {
+    const file = imageInput.files[0];
+    if (file.size > 10 * 1024 * 1024) {
+      return new Error('File too large');
+    }
+    try {
+      const base64Image = await toBase64(file);
+      b64 = base64Image;
+    } catch (error) {
+      return error;
+    }
+  } else {
+    return new Error('No file selected');
+  }
+  
+  const url = 'https://api.ocr.space/parse/image';
+  let data = new FormData()
+  data.set("base64Image", b64)
+  data.set("apikey", 'K84105813588957')
 
- return new Promise((resolve, reject) => {
-
-   const reader = new FileReader();
-
-   reader.readAsDataURL(file);
-
-   reader.onload = () => resolve(reader.result);
-
-   reader.onerror = error => reject(error);
-
- });
-
-}
-
-function OCR(image) {
-
-// Define the URL and data
-
-const url = 'https://api.ocr.space/parse/image’;
-
-const data = {
-
-  apikey: ‘K84105813588957’
-
-  base64Image: toBase64(image)
-
-};
-
-// Define the options
-
-const options = {
-
-  method: 'POST',
-
-  headers: {
-
-    'Content-Type': 'application/json'
-
-  },
-
-  body: JSON.stringify(data)
-
-};
-
-
-// Send the request
-
-fetch(url, options)
-
-  .then(response => response.json())
-
-  .then(data => return data)
-
-  .catch(error => return {error:true});
-
+  try {
+    const response = await fetch(url, {method: 'POST', body: data});
+    const json = await response.json();
+    return json['ParsedResults'][0]['ParsedText'].replaceAll('\r\n', ' ');
+  } catch (error) {
+    return error;
+  }
 }
