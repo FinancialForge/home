d3439eece90853df4173c4e2d64c24f321301e15
LeonardLamparter <88286394+leolamparter@users.noreply.github.com>
2025-01-08 10:42:21 -0400

    Put All Code Through DOMContentLoaded listener event

--- add-receipt.js
+++ add-receipt.js
@@ -5,23 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
     openSidebar.addEventListener('click', function() {
         sidebar.classList.toggle('active');
     });
-});
 
-document.getElementById('img').addEventListener('change', function() {
-    var fileName = this.files[0] ? this.files[0].name : "No file selected";
-    document.querySelector('.file-name').textContent = fileName;
-});
-
-let imgOBJ = document.getElementById('img')
-let motOBJ = document.getElementById('mot')
-let catOBJ = document.getElementById('cat')
-
-document.getElementById('go').addEventListener(e => {
-    e.preventDefault()
-    OCR(imaOBJ).then(txt => extractTotal(txt)).then(total => {
-        let month = motOBJ.value
-        let category = catOBJ.value
-
-        console.log({total, month, category})
-    })
+    document.getElementById('img').addEventListener('change', function() {
+        var fileName = this.files[0] ? this.files[0].name : "No file selected";
+        document.querySelector('.file-name').textContent = fileName;
+    });
+    
+    document.getElementById('go').addEventListener(e => {
+        e.preventDefault()
+        let imgOBJ = document.getElementById('img')
+        let motOBJ = document.getElementById('mot')
+        let catOBJ = document.getElementById('cat')
+        OCR(imaOBJ).then(txt => extractTotal(txt)).then(total => {
+            let month = motOBJ.value
+            let category = catOBJ.value
+    
+            console.log({total, month, category})
+        })
+    });
 });
